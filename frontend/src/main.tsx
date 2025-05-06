d2be62def0f2ba1bd698d29dd7584b43a7ec83a4
mogaya <mogayainnocent@gmail.com>
2025-05-06 17:44:17 +0300

    Refactor main.tsx to integrate RouterProvider with AppRoutes and apply Chakra UI theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { RouterProvider } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes.tsx";
+import theme from "./theme.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={AppRoutes} />
     </ChakraProvider>
   </StrictMode>
 );
