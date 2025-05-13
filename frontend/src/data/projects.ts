ff59829bb7161a2b833b0b3d0f0541c15c4e1487
mogaya <mogayainnocent@gmail.com>
2025-05-13 16:15:12 +0300

    Add projects data file to define featured projects with descriptions and technologies

---
+++ frontend/src/data/projects.ts
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    id: 1,
+    title: "TecTally - Asset Manager",
+    description:
+      "A mobile app to manage IT assets efficiently for institutions.",
+    tech: ["Flutter", "Dart", "PHP", "MySQL"],
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    liveUrl: "#",
+    sourceUrl: "#",
+  },
+
+  {
+    id: 2,
+    title: "NurseShelf - Subscription based digital library",
+    description:
+      "a digital library that provides nursing students and professionals with categorized study resources, including notes and revision papers.",
+    tech: [
+      "React",
+      "Typescript",
+      "Chakra UI",
+      "Django",
+      "PostgreSQL",
+      "Docker",
+    ],
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    liveUrl: "#",
+    sourceUrl: "#",
+  },
+
+  {
+    id: 3,
+    title: "Escape With Us – Travel Agency Website",
+    description:
+      "A WordPress-powered website for a travel agency, showcasing tour packages, destinations, and travel services.",
+    tech: ["WordPress", "Elementor", "PHP", "CSS"],
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    liveUrl: "#",
+    sourceUrl: "#",
+  },
+];
