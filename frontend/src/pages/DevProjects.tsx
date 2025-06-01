ee4ed189e0986bea33d227609f3de19a03980323
mogaya <mogayainnocent@gmail.com>
2025-06-01 17:58:38 +0300

    Remove unnecessary React imports from layout and project components

--- frontend/src/pages/DevProjects.tsx
+++ frontend/src/pages/DevProjects.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const DevProjects = () => {
   return <div>DevProjects</div>;
 };
--- frontend/src/pages/GraphicProjects.tsx
+++ frontend/src/pages/GraphicProjects.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const GraphicProjects = () => {
   return <div>GraphicProjects</div>;
 };
