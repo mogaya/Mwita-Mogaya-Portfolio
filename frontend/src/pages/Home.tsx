3c296f3a9df2f680e1eb2830f51ddb93562bc864
mogaya <mogayainnocent@gmail.com>
2025-06-02 17:11:00 +0300

    Refactor Home component to clean up commented sections and maintain structure

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,17 @@
-import AboutMe from "../components/Home/AboutMe";
-import Awards from "../components/Home/Awards";
 import Contact from "../components/Home/Contact";
 import Hero from "../components/Home/Hero";
 import Projects from "../components/Home/Projects";
 import Services from "../components/Home/Services";
-import Skills from "../components/Home/Skills";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Projects />
       <Services />
-      <Skills />
-      <Awards />
-      <AboutMe />
+      <Projects />
+      {/* <Skills /> */}
+      {/* <Awards /> */}
+      {/* <AboutMe /> */}
       <Contact />
     </>
   );
