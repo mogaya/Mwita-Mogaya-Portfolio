9a6dbae66c96104ca216153e9f5d1a050cacd806
mogaya <mogayainnocent@gmail.com>
2025-06-04 19:11:03 +0300

    Enhance Footer component with social media links and updated styling

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -1,5 +1,100 @@
+import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
+import { FaInstagram, FaSquareGithub, FaWhatsapp } from "react-icons/fa6";
+import { TfiLinkedin } from "react-icons/tfi";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      as={"footer"}
+      w={"100vw"}
+      transition={"all 0.2s"}
+      py={6}
+      // bgColor={"secondary"}
+    >
+      <Box maxW={"1200px"} mx={"auto"} px={4}>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          justify={"space-between"}
+          alignItems={{ base: "start", md: "center" }}
+        >
+          <Box mb={{ base: 6, md: 0 }}>
+            <Text as={"h3"} fontSize={"xl"} fontWeight={"bold"}>
+              Mwita Mogaya
+            </Text>
+            <Text color={"foreground"} fontWeight={"medium"} opacity={0.8}>
+              Full Stack Developer & Graphic Designer
+            </Text>
+          </Box>
+
+          <Flex flexDirection={"row"} gap={3} justifyContent={"center"}>
+            <IconButton
+              as={"a"}
+              href={"https://wa.me/254704379546"}
+              aria-label={"linkedin"}
+              icon={<FaWhatsapp style={{ fontSize: 30 }} />}
+              backgroundColor={"transparent"}
+              _focus={{ border: "none", color: "primary" }}
+              _hover={{ border: "none", color: "primary" }}
+              borderRadius={"full"}
+              color={"foreground"}
+            />
+            <IconButton
+              as={"a"}
+              href={"https://github.com/mogaya"}
+              aria-label={"linkedin"}
+              icon={
+                <FaSquareGithub
+                  style={{
+                    fontSize: 30,
+                  }}
+                />
+              }
+              backgroundColor={"transparent"}
+              borderRadius={"full"}
+              _focus={{ border: "none", color: "primary" }}
+              _hover={{ border: "none", color: "primary" }}
+              color={"foreground"}
+            />
+            <IconButton
+              as={"a"}
+              href={"https://www.instagram.com/_mogaya/"}
+              aria-label={"linkedin"}
+              icon={<FaInstagram style={{ fontSize: 31 }} />}
+              backgroundColor={"transparent"}
+              borderRadius={"full"}
+              _focus={{ border: "none", color: "primary" }}
+              _hover={{ border: "none", color: "primary" }}
+              color={"foreground"}
+            />
+            <IconButton
+              as={"a"}
+              href={"https://www.linkedin.com/in/mwita-mogaya/"}
+              aria-label={"linkedin"}
+              borderRadius={"full"}
+              icon={<TfiLinkedin style={{ fontSize: 30 }} />}
+              backgroundColor={"transparent"}
+              _focus={{ border: "none", color: "primary" }}
+              _hover={{ border: "none", color: "primary" }}
+              color={"foreground"}
+            />
+          </Flex>
+        </Flex>
+        <Text
+          as={"p"}
+          // fontSize={"md"}
+          px={2}
+          textAlign={"end"}
+          color={"foreground"}
+          fontWeight={"medium"}
+          opacity={0.8}
+          mt={6}
+        >
+          {" "}
+          &copy; {new Date().getFullYear()} mmogaya.com. All rights reserved
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
